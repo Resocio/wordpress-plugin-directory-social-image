5fc4b22f9692ab9dbbb9df2dc306ab2c6b40d424
Philippe Bernard <philippe.bernard@gmail.com>
2021-10-23 19:06:29 +0200

    Minimalist API call

--- slug-to-image-data.js
+++ slug-to-image-data.js
@@ -1,9 +1,14 @@
-exports.slugToImageData = (slug) => {
+const fetch = require('node-fetch');
+
+exports.slugToImageData = async (slug) => {
+  const response = await fetch(`https://api.wordpress.org/plugins/info/1.0/${slug}.json`);
+  const data = await response.json();
+
   return {
     template: 'plugin',
     values: {
-      title: 'Hello',
-      description: 'World!'
+      title: data.name,
+      description: 'TODO'
     }
   }
 }
